a64ba300e4492060fdc334903271cfafd636a7dc
jonDuke <jonduke90@gmail.com>
2020-03-09 15:59:42 -0700

    adds __init__.py and modifies my_script to test module functions

---
+++ my_lambdata/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+# treat the surrounding folder like a module
+# so files in it can be imported
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,5 +1,20 @@
 # my_lambdata/my_script.py
 
-from my_lambdata.my_mod import say_hello
+import my_lambdata.my_mod as my_mod
+import pandas as pd
 
-say_hello()
+df = pd.DataFrame({'A':range(10), 'B':range(10,20)})
+
+print("original dataframe:")
+print(df.head(10))
+
+print('\nadding a column:')
+new_col = range(0,20,2)
+df = my_mod.add_column(new_col, df, 'C')
+print(df.head(10))
+
+print('\nperforming a train/val/test split:')
+train, val, test = my_mod.train_val_test_split(df)
+print('train:', train.head())
+print('val:', val.head())
+print('test:', test.head())
